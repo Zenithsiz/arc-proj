a73d9f84286bd799a04f2321f16b42f5e0dcdab2
Filipe Rodrigues <filipejacintorodrigues1@gmail.com>
2023-10-02 03:31:47 +0100

    Fixed formatting of time not correcting displaying micro and nano seconds.

--- src/arc_proj/util.py
+++ src/arc_proj/util.py
@@ -23,24 +23,26 @@ def try_index_dict(container: Mapping[U, T], key: U) -> T | None:
 
 def fmt_time(s: float) -> str:
 	"""
-	Formats time in seconds in the closes unit.
+	Formats time in seconds in the closest unit.
 	"""
+	assert(s >= 0)
+
 	hours   = s // 3600
 	mins    = s // 60   - 60 * hours
 	seconds = s // 1    - 60 * mins  - 3600 * hours
-	milliseconds = 1e3 * s - 1e0 * int(seconds)
-	microseconds = 1e6 * s - 1e3 * int(milliseconds)
-	nanoseconds  = 1e9 * s - 1e6 * int(microseconds)
+	milliseconds = 1e3 * (s - seconds)
+	microseconds = 1e6 * (s - seconds)
+	nanoseconds  = 1e9 * (s - seconds)
 
 	if hours > 0:
-		return f"{hours}h{mins}m{s%60:.2f}s"
+		return f"{hours:.0f}h{mins:.0f}m{s%60:.2f}s"
 	elif mins > 0:
-		return f"{mins}m{s%60:.2f}s"
+		return f"{mins:.0f}m{s%60:.2f}s"
 	elif seconds > 0:
 		return f"{s:.2f}s"
-	elif milliseconds > 0:
+	elif milliseconds >= 1.0:
 		return f"{milliseconds:.2f}ms"
-	elif microseconds > 0:
+	elif microseconds >= 1.0:
 		return f"{microseconds:.2f}μs"
-	elif nanoseconds > 0:
+	else:
 		return f"{nanoseconds:.2f}ns"
